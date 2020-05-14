b6c1a0f5c480fdd81bce1adc598360dc0a7d923c
vipul jain <vipul@pietechsolution.com>
2020-05-14 10:57:30 +0000

    Update profile.dart

--- lib/screens/tab/profile.dart
+++ lib/screens/tab/profile.dart
@@ -7,6 +7,7 @@ import 'package:readymadeGroceryApp/screens/authe/login.dart';
 import 'package:readymadeGroceryApp/screens/drawer/address.dart';
 import 'package:readymadeGroceryApp/screens/orders/orders.dart';
 import 'package:readymadeGroceryApp/screens/tab/editprofile.dart';
+import 'package:readymadeGroceryApp/service/constants.dart';
 import 'package:readymadeGroceryApp/service/initialize_i18n.dart';
 import 'package:readymadeGroceryApp/service/localizations.dart';
 import 'package:readymadeGroceryApp/style/style.dart';
@@ -24,7 +25,7 @@ class Profile extends StatefulWidget {
   final String locale;
   Profile({Key key, this.locale, this.localizedValues}) : super(key: key);
 
-  @override
+  &#64;override
   _ProfileState createState() => _ProfileState();
 }
 
@@ -50,13 +51,13 @@ class _ProfileState extends State<Profile> {
   Map<String, Map<String, String>> localizedValues;
   var userData;
 
-  @override
+  &#64;override
   void initState() {
     getToken();
     super.initState();
   }
 
-  @override
+  &#64;override
   void dispose() {
     super.dispose();
   }
@@ -292,7 +293,7 @@ class _ProfileState extends State<Profile> {
         });
   }
 
-  @override
+  &#64;override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFDFDFD),
@@ -316,263 +317,4 @@ class _ProfileState extends State<Profile> {
         controller: _refreshController,
         onRefresh: () {
           setState(() {
-            getToken();
-          });
-        },
-        child: isGetTokenLoading
-            ? SquareLoader()
-            : token == null
-                ? Login(
-                    locale: widget.locale,
-                    localizedValues: widget.localizedValues,
-                    isProfile: true)
-                : ListView(
-                    children: <Widget>[
-                      InkWell(
-                        onTap: () {
-                          var result = Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EditProfile(
-                                  locale: widget.locale,
-                                  localizedValues: widget.localizedValues,
-                                  userInfo: userInfo),
-                            ),
-                          );
-                          result.then((value) => getToken());
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          margin: EdgeInsets.only(
-                            top: 20,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Flexible(
-                                flex: 2,
-                                fit: FlexFit.tight,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(27)),
-                                        boxShadow: [
-                                          BoxShadow(
-                                              color: Colors.black
-                                                  .withOpacity(0.29),
-                                              blurRadius: 6)
-                                        ]),
-                                    height: 90.0,
-                                    width: 91.0,
-                                    child: userInfo == null ||
-                                            userInfo['profilePic'] == null
-                                        ? Center(
-                                            child: new Container(
-                                              width: 200.0,
-                                              height: 200.0,
-                                              decoration: new BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(27.0),
-                                                image: new DecorationImage(
-                                                  fit: BoxFit.fill,
-                                                  image: new AssetImage(
-                                                      'lib/assets/images/profile.png'),
-                                                ),
-                                              ),
-                                            ),
-                                          )
-                                        : Center(
-                                            child: new Container(
-                                              width: 200.0,
-                                              height: 200.0,
-                                              decoration: new BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(20.0),
-                                                image: new DecorationImage(
-                                                  fit: BoxFit.fill,
-                                                  image: new NetworkImage(
-                                                      userInfo['profilePic']),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                  ),
-                                ),
-                              ),
-                              Flexible(
-                                flex: 4,
-                                fit: FlexFit.tight,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          right: .0, bottom: 6.0),
-                                      child: Text(
-                                        '${userInfo['firstName'] ?? ""} ${userInfo['lastName'] ?? ""}',
-                                        style: textBarlowMediumBlack(),
-                                      ),
-                                    ),
-                                    SizedBox(height: 6),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                          '${userInfo['email'] ?? ""}',
-                                          style: textbarlowmedium(),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(height: 6),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 5.0, right: .0),
-                                      child: Text(
-                                        '${userInfo['mobileNumber'] ?? ""}',
-                                        style: textbarlowmedium(),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Flexible(
-                                child: Row(
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: EdgeInsets.only(top: 45),
-                                          child: SvgPicture.asset(
-                                              'lib/assets/icons/editt.svg'),
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      InkWell(
-                        onTap: () {
-                          var result = Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Address(
-                                locale: widget.locale,
-                                localizedValues: widget.localizedValues,
-                              ),
-                            ),
-                          );
-                          result.then((value) => getToken());
-                        },
-                        child: Container(
-                          height: 55,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFF7F7F7),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10.0,
-                                    bottom: 10.0,
-                                    left: 20.0,
-                                    right: 20.0),
-                                child: Text(
-                                  MyLocalizations.of(context).address,
-                                  style: textBarlowMediumBlack(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      InkWell(
-                        onTap: () {
-                          selectLanguagesMethod();
-                        },
-                        child: Container(
-                          height: 55,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFF7F7F7),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10.0,
-                                    bottom: 10.0,
-                                    left: 20.0,
-                                    right: 20.0),
-                                child: Text(
-                                  MyLocalizations.of(context).selectLanguage,
-                                  style: textBarlowMediumBlack(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          var result = Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Orders(
-                                locale: widget.locale,
-                                localizedValues: widget.localizedValues,
-                                userID: userID,
-                              ),
-                            ),
-                          );
-                          result.then((value) => getToken());
-                        },
-                        child: Container(
-                          height: 55,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFF7F7F7),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10.0,
-                                    bottom: 10.0,
-                                    left: 20.0,
-                                    right: 20.0),
-                                child: Text(
-                                  MyLocalizations.of(context).orderHistory,
-                                  style: textBarlowMediumBlack(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                    ],
-                  ),
-      ),
-    );
-  }
-}
+            getTok...</Widget></dynamic></dynamic></dynamic></dynamic></dynamic></dynamic></dynamic></dynamic></dynamic></Widget></String></Profile>
