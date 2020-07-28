b848f6fcacd6d05fcdcee470e0c2984bd2a1d280
Nitish Kumar <nitishk1316@gmail.com>
2020-07-28 07:32:43 +0200

    Added interceptor

--- lib/service/intercepter.dart
+++ lib/service/intercepter.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:readymadeGroceryApp/service/common.dart';
 
@@ -20,5 +24,24 @@ class ApiInterceptor implements InterceptorContract {
   }
 
   @override
-  Future<ResponseData> interceptResponse({ResponseData data}) async => data;
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    //print(data.statusCode);
+    //print(json.decode(data.body));
+    var errorData = json.decode(data.body);
+    if (data.statusCode == 400) {
+      var msg = '';
+      for(int i = 0, l = errorData['errors'].length; i < l; i++) { msg += errorData['errors'][i] + '\n'; } 
+      Fluttertoast.showToast(
+          msg: msg,
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      return Future.error('Unexpected error 😢');
+    }
+    return data;
+  }
 }
