41378e43385e9f740f07eabaa58b7b33efbdcad3
vipul8815 <vipul@pietechsolution.com>
2020-08-17 16:19:55 +0530

    remove mylocalization in app bar

--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -5,20 +5,15 @@ import 'package:readymadeGroceryApp/style/style.dart';
 
 Widget appBarPrimary(BuildContext context, title) {
   return GFAppBar(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.only(
-        bottomLeft: Radius.circular(20),
-        bottomRight: Radius.circular(20),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
       ),
-    ),
-    title: Text(
-      MyLocalizations.of(context).getLocalizations(title),
-      style: textbarlowSemiBoldBlack(),
-    ),
-    centerTitle: true,
-    backgroundColor: primary,
-    iconTheme: IconThemeData(color: Colors.black),
-  );
+      title: Text(MyLocalizations.of(context).getLocalizations(title),
+          style: textbarlowSemiBoldBlack()),
+      centerTitle: true,
+      backgroundColor: primary,
+      iconTheme: IconThemeData(color: Colors.black));
 }
 
 Widget appBarWhite(BuildContext context, title, bool changeUi,
@@ -26,28 +21,21 @@ Widget appBarWhite(BuildContext context, title, bool changeUi,
   return GFAppBar(
     title: changeUi
         ? title
-        : Text(
-            MyLocalizations.of(context).getLocalizations(title),
-            style: textbarlowSemiBoldBlack(),
-          ),
+        : Text(MyLocalizations.of(context).getLocalizations(title),
+            style: textbarlowSemiBoldBlack()),
     centerTitle: true,
     backgroundColor: bg,
     iconTheme: IconThemeData(color: Colors.black),
-    actions: <Widget>[
-      actionTrueOrFalse ? actionProcess : Container(),
-    ],
+    actions: <Widget>[actionTrueOrFalse ? actionProcess : Container()],
   );
 }
 
 Widget appBarTransparent(BuildContext context, title) {
   return GFAppBar(
-    title: Text(
-      MyLocalizations.of(context).getLocalizations(title),
-      style: textbarlowSemiBoldBlack(),
-    ),
-    centerTitle: true,
-    backgroundColor: Colors.transparent,
-    iconTheme: IconThemeData(color: Colors.black),
-    elevation: 0,
-  );
+      title: Text(MyLocalizations.of(context).getLocalizations(title),
+          style: textbarlowSemiBoldBlack()),
+      centerTitle: true,
+      backgroundColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.black),
+      elevation: 0);
 }
